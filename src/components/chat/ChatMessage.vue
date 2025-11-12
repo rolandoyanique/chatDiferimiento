905046b2b56601d1ee3e2cc919e69bf5eb48f1cb
ryanique <ryanique@corex.services>
2025-11-12 15:39:43 -0400

    Se realizo la adicion del servicio para consumir

--- src/components/chat/ChatMessage.vue
+++ src/components/chat/ChatMessage.vue
@@ -1,13 +1,18 @@
 <template>
-  <div class="flex-1 overflow-y-auto p-4">
-    <ChatBubble v-for="msg in message" :key="msg.id" v-bind="msg" />
+  <div ref="chatRef" class="flex-1 overflow-y-auto p-4">
+    <div class="flex-1 overflow-y-auto p-4">
+      <ChatBubble v-for="msg in message" :key="msg.id" v-bind="msg" />
+    </div>
   </div>
 </template>
 <script lang="ts" setup>
 import type { ChatMessages } from '@/interfaces/chat-message.interface';
 import ChatBubble from './ChatBubble.vue';
+import { ref } from 'vue';
 interface Props {
   message: ChatMessages[];
 }
 defineProps<Props>();
+
+const chatRef = ref<HTMLElement | null>(null);
 </script>
--- src/composables/useChat.ts
+++ src/composables/useChat.ts
@@ -1,13 +1,30 @@
+import { sleep } from '@/helpers/sleep';
 import type { ChatMessages } from '@/interfaces/chat-message.interface';
+import type { YesNoResponse } from '@/interfaces/yes-no.response';
 import { ref } from 'vue';
 
 export const useChat = () => {
   const messages = ref<ChatMessages[]>([]);
-  const onMessage = (text: string) => {
+  const getHerResponse = async () => {
+    const resp = await fetch('https://yesno.wtf/api');
+    const data = (await resp.json()) as YesNoResponse;
+    return data;
+  };
+  const onMessage = async (text: string) => {
+    if (text.length === 0) return;
     messages.value.push({
       id: new Date().getTime(),
       isMine: true,
       message: text,
+    });
+    if (!text.endsWith('?')) return;
+    await sleep(1.5);
+    const { answer, image } = await getHerResponse();
+    messages.value.push({
+      id: new Date().getTime(),
+      isMine: false,
+      message: answer,
+      image: image,
     });
   };
   return {
